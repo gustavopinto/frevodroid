110527f201e9998bfd507f72bac5332c95423196
gustavopinto <gustavohenrique.86@gmail.com>
2012-04-15 11:20:07 -0300

    novos imagens

--- gen/br/com/entropie/frevodroid/R.java
+++ gen/br/com/entropie/frevodroid/R.java
@@ -18,13 +18,15 @@ public final class R {
     }
     public static final class id {
         public static final int imgIcon=0x7f050000;
-        public static final int map_view=0x7f050003;
+        public static final int map_view=0x7f050004;
+        public static final int textView1=0x7f050003;
         public static final int txtDayOfWeek=0x7f050001;
         public static final int txtDescription=0x7f050002;
     }
     public static final class layout {
         public static final int calendar=0x7f030000;
-        public static final int maps=0x7f030001;
+        public static final int event=0x7f030001;
+        public static final int maps=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
